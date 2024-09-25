ab465d3c6b1ca09e7083bfac437423c7b9159a30
Rishabh Khanka <godzopgamer@gmail.com>
2024-09-25 18:55:01 +0530

    Add files via upload

---
+++ fee.py
@@ -0,0 +1,10 @@
+﻿A = int(input("A : "))
+G = input("B : ")
+if((A == 1 or A == 2) and G == "X"):
+    print("fee is 100")
+if((A == 3 or A == 4) and G == "Y"):
+    print("fee is 200")
+if((A == 5 and G == "Z")):
+    print("fee is 300")
+else:
+    print("no fee") 
