897ed1adfb321de8c1e64595d12a352fc6e915f0
Rishabh Khanka <godzopgamer@gmail.com>
2024-10-08 20:23:01 +0530

    Add files via upload

---
+++ traffic.py
@@ -0,0 +1,10 @@
+﻿light= input("light color: ")
+
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("look")
+elif(light == "green"):
+    print("go")
+else:
+    print("light is broken")
